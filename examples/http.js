06af2fa9ecc4944c5b2af1f669abaebdeb43be71
Oleg Podsechin <oleg.podsechin@ionsquare.com>
2011-09-03 04:10:09 +0300

    simplified http proxy example

--- examples/http.js
+++ examples/http.js
@@ -1,19 +1,13 @@
 /**
- * @fileOverview HTTP proxy example. Proxies responses from Google. Note that
- *               rather than first getting the response, storing it in a string
- *               and then passing it on, we are using the Stream directly and
- *               are piping data through as it arrives without unnecessary
- *               buffering.
+ * @fileOverview HTTP proxy example. Note that rather than first getting the
+ * response, storing it in a string and then passing it on, we are using the
+ * Stream directly and are piping data through as it arrives without unnecessary
+ * buffering.
  */
 
 var HttpClient = require('httpclient').HttpClient;
 
-exports.app = function(request) {
-	return {
-		status: 200,
-		headers: {},
-		body: new HttpClient({
-			url: 'http://google.com'
-		}).finish().body
-	};
+exports.app = function(req) {
+	req.url = 'http://nodejs.org';
+	return new HttpClient(req).finish();
 };
