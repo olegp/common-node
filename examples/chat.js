4209aa853bf9793d6478ee7be15455788d7dec32
Oleg Podsechin <oleg.podsechin@ionsquare.com>
2011-09-07 03:11:04 +0300

    removed textstream

--- examples/chat.js
+++ examples/chat.js
@@ -3,22 +3,19 @@
  * to port 8080.
  */
 var Socket = require('socket').Socket;
-var TextStream = require('io').TextStream;
 
 var clients = [], server = new Socket().bind('localhost', 8080);
 while(true) {
-	var socket = server.accept(), client = new TextStream(socket.getStream());
+	var client = server.accept().getStream();
 	clients.push(client);
 	spawn(function() {
 		var stream = client, line;
-		while((line = stream.readLine()).length) {
-			if(line.length > 1) {
+		while((line = stream.read(null)).length) {
 				clients.forEach(function(client) {
 					if(stream != client) {
 						client.write(line);
 					}
 				});
-			}
 		}
 		clients.splice(clients.indexOf(stream), 1);
 	});
