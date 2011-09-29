561551666a51050afc799ba19a54722d9757268b
Oleg Podsechin <oleg.podsechin@ionsquare.com>
2011-09-29 10:44:09 +0200

    bring node example to be in line with common-node (clear buffer after creation)

--- benchmarks/buffer-alloc/common-node.js
+++ benchmarks/buffer-alloc/common-node.js
@@ -3,7 +3,7 @@ var ByteArray = require('binary').ByteArray;
 exports.app = function(request) {
 	var n = 1024, b = new ByteArray(n);
 	for( var i = 1; i < 50; i++)
-		b = new ByteArray(n);
+		b = new ByteArray(n, true);
 	return {
 		status: 200,
 		headers: {
--- benchmarks/buffer-alloc/node.js
+++ benchmarks/buffer-alloc/node.js
@@ -3,8 +3,10 @@ var Buffer = require('buffer').Buffer;
 
 http.createServer(function(req, res) {
 	var n = 1024, b = new Buffer(n);
-	for( var i = 1; i < 50; i++)
+	for( var i = 1; i < 50; i++) {
 		b = new Buffer(n);
+		b.fill(0);
+  }
 	res.writeHead(200, {
 		"Content-Type": "text/plain"
 	});
