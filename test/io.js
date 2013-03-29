ae3dd51f0e728b40cae23b3e49ad06283c029e23
alexlamsl <alexlamsl@gmail.com>
2013-03-29 00:27:41 +0000

    LINT

--- test/io.js
+++ test/io.js
@@ -7,64 +7,64 @@ var TextStream = io.TextStream;
 var ByteArray = require('../lib/binary').ByteArray;
 
 function getStream(file) {
-	return new Stream(fs.createReadStream(file));
+  return new Stream(fs.createReadStream(file));
 }
 
 function getContents(file) {
-	return fs.readFileSync(file, "utf8");
+  return fs.readFileSync(file, "utf8");
 }
 
 exports.testReadFixed = function() {
-	var io = getStream('./lib/io.js');
-	var bytes = io.read(7);
-	assert.strictEqual(bytes.length, 7);
+  var io = getStream('./lib/io.js');
+  var bytes = io.read(7);
+  assert.strictEqual(bytes.length, 7);
 
-	bytes = io.read(5);
-	assert.strictEqual(bytes.length, 5);
-	// assert.strictEqual(bytes.decodeToString(), 'include');
+  bytes = io.read(5);
+  assert.strictEqual(bytes.length, 5);
+  // assert.strictEqual(bytes.decodeToString(), 'include');
 };
 
 exports.testReadIndefinite = function() {
-	var resource = getContents('./lib/assert.js');
-	var io = getStream('./lib/assert.js');
-	var bytes = io.read();
-	assert.strictEqual(bytes.length, resource.length);
-	assert.strictEqual(bytes.decodeToString(), resource);
+  var resource = getContents('./lib/assert.js');
+  var io = getStream('./lib/assert.js');
+  var bytes = io.read();
+  assert.strictEqual(bytes.length, resource.length);
+  assert.strictEqual(bytes.decodeToString(), resource);
 };
 
 exports.testReadBlock = function() {
-	// var resource = getContents('./lib/assert.js');
-	// var io = getStream('./lib/assert.js');
-	// var bytes = io.read(null);
-	// assert.notEqual(bytes.length, resource.length);
+  // var resource = getContents('./lib/assert.js');
+  // var io = getStream('./lib/assert.js');
+  // var bytes = io.read(null);
+  // assert.notEqual(bytes.length, resource.length);
 };
 
 exports.testStreamForEach = function() {
-	var resource = getContents('./lib/assert.js');
-	var io = getStream('./lib/assert.js');
-	var str = "";
-	var read = 0;
-	io.forEach(function(data) {
-		read += data.length;
-		str += data.decodeToString();
-	});
-	assert.strictEqual(read, resource.length);
-	assert.strictEqual(str, resource);
+  var resource = getContents('./lib/assert.js');
+  var io = getStream('./lib/assert.js');
+  var str = "";
+  var read = 0;
+  io.forEach(function(data) {
+    read += data.length;
+    str += data.decodeToString();
+  });
+  assert.strictEqual(read, resource.length);
+  assert.strictEqual(str, resource);
 };
 
 exports.testReadInto = function() {
-	var resource = getContents('./lib/assert.js');
-	var io = getStream('./lib/assert.js');
-	var bytes = new ByteArray(7);
-	io.readInto(bytes);
-	assert.equal(bytes.decodeToString(), resource.substring(0, 7));
+  var resource = getContents('./lib/assert.js');
+  var io = getStream('./lib/assert.js');
+  var bytes = new ByteArray(7);
+  io.readInto(bytes);
+  assert.equal(bytes.decodeToString(), resource.substring(0, 7));
 };
 
 exports.testReadLine = function() {
   var resource = getContents('./lib/assert.js');
-	var io = new TextStream(getStream('./lib/assert.js'));
-	var lines = io.readLines();
-	assert.equal(lines.length, resource.replace(/\n$/, "").split(/\n/).length);
+  var io = new TextStream(getStream('./lib/assert.js'));
+  var lines = io.readLines();
+  assert.equal(lines.length, resource.replace(/\n$/, "").split(/\n/).length);
 };
 
 exports.testWrite = test.fs(1, function(file) {
@@ -77,5 +77,5 @@ exports.testWrite = test.fs(1, function(file) {
 });
 
 if (require.main === module) {
-	require("../lib/test").run(exports);
+  require("../lib/test").run(exports);
 }
