5b7036dee04e059673ba8fdb8c6bde0c790fb949
alexlamsl <alexlamsl@gmail.com>
2013-03-29 00:35:28 +0000

    alternative benchmark script that requires no addition dependencies

---
+++ benchmarks/run.js
@@ -0,0 +1,41 @@
+var system = require("system");
+var get = require("ringo/httpclient").get;
+
+var running = true;
+
+function poke() {
+  while (running) {
+    try {
+      get('http://localhost:8080/');
+      count++;
+    } catch (e) {
+      error++;
+    }
+  }
+}
+
+if (require.main === module) {
+  var benchmark = 'benchmarks/' + system.args[2] + '/common-node.js';
+  console.log('Launching "' + benchmark + '"...');
+  require('child_process').spawn('node', ['lib/run', benchmark]);
+  console.log('Attacking...');
+
+  var past = Date.now();
+  var count = 0;
+  var error = 0;
+  for (var i = 0; i < 5; i++) {
+    spawn(poke);
+  }
+
+  var previous = 0;
+  setInterval(function() {
+    var now = Date.now();
+    var next = count;
+    console.log((1e3 * (next - previous) / (now - past)).toFixed(2), ' req/s (' + error + ')');
+    past = now;
+    previous = next;
+  }, 10000).unref();
+
+  system.stdin.readLine();
+  running = false;
+}
