b45b37a90da7bcfcba35f0e2d5f4ea565ea1da9f
alexlamsl <alexlamsl@gmail.com>
2013-03-29 00:33:38 +0000

    close #25

--- test/io.js
+++ test/io.js
@@ -17,8 +17,9 @@ function getContents(file) {
   return fs.readFileSync(file, "utf8");
 }
 
+var resource = getContents('./lib/assert.js');
+
 exports.testStreamReadFixed = function() {
-  var resource = getContents('./lib/assert.js');
   var io = getStream('./lib/io.js');
   var bytes = io.read(7);
   assert.strictEqual(bytes.length, 7);
@@ -29,7 +30,6 @@ exports.testStreamReadFixed = function() {
 };
 
 exports.testStreamReadIndefinite = function() {
-  var resource = getContents('./lib/assert.js');
   var io = getStream('./lib/assert.js');
   var bytes = io.read();
   assert.strictEqual(bytes.length, resource.length);
@@ -37,7 +37,6 @@ exports.testStreamReadIndefinite = function() {
 };
 
 exports.testStreamForEach = function() {
-  var resource = getContents('./lib/assert.js');
   var io = getStream('./lib/assert.js');
   var str = "";
   var read = 0;
@@ -50,18 +49,10 @@ exports.testStreamForEach = function() {
 };
 
 exports.testStreamReadInto = function() {
-  var resource = getContents('./lib/assert.js');
   var io = getStream('./lib/assert.js');
   var bytes = new ByteArray(7);
   io.readInto(bytes);
   assert.equal(bytes.decodeToString(), resource.substring(0, 7));
-};
-
-exports.testTextStreamReadLine = function() {
-  var resource = getContents('./lib/assert.js');
-  var io = new TextStream(getStream('./lib/assert.js'));
-  var lines = io.readLines();
-  assert.equal(lines.length, resource.replace(/\n$/, "").split(/\n/).length);
 };
 
 exports.testStreamWrite = test.fs(1, function(file) {
@@ -69,72 +60,153 @@ exports.testStreamWrite = test.fs(1, function(file) {
   io.write(new ByteArray('test'));
   io.flush();
   io.close();
-  var resource = getContents(file);
-  assert.strictEqual('test', resource);
+  assert.strictEqual('test', getContents(file));
 });
 
-exports.testMemoryStream = function() {
-  var resource = getContents('./lib/assert.js');
+function checkRW(io, readable, writable) {
+  assert.strictEqual(io.readable(), readable);
+  assert.strictEqual(io.writable(), writable);
+}
 
-  function checkRW(readable, writable) {
-    assert.strictEqual(io.readable(), readable);
-    assert.strictEqual(io.writable(), writable);
-  }
+function checkCursors(io, position, length) {
+  assert.strictEqual(io.position, position);
+  assert.strictEqual(io.length, length);
+}
 
-  function checkCursors(position, length) {
-    assert.strictEqual(io.position, position);
-    assert.strictEqual(io.length, length);
-  }
-
-  function noWrite() {
-    assert.throws(function() {
-      io.write('');
-    });
-  }
-
-  function write() {
-    io.write(resource);
-    checkCursors(resource.length, resource.length);
-    io.write(new ByteArray(resource));
-    checkCursors(resource.length * 2, resource.length * 2);
-    io.position = 0;
-    io.write(new ByteString(resource));
-    checkCursors(resource.length, resource.length * 2);
-  }
-
-  function read() {
-    var position = io.position;
-    var length = io.length;
-    var bytes = io.read(resource.length);
-    assert.strictEqual(bytes.length, resource.length);
-    assert.strictEqual(bytes.decodeToString(), resource);
-    checkCursors(position + resource.length, length);
-  }
-
-  var io;
-  [new MemoryStream(), new MemoryStream(resource.length)].forEach(function(stream) {
-    io = stream;
-    checkRW(true, true);
-    checkCursors(0, 0);
-    io.position = 42;
-    checkCursors(0, 0);
-    write();
-    read();
-    io.close();
-    noWrite();
+function noWrite(io) {
+  assert.throws(function() {
+    io.write('');
   });
-  io = new MemoryStream(new ByteArray(resource));
-  checkRW(true, true);
-  checkCursors(0, resource.length);
-  write();
-  read();
+}
+
+function write(io) {
+  io.write(resource);
+  checkCursors(io, resource.length, resource.length);
+  io.write(new ByteArray(resource));
+  checkCursors(io, resource.length * 2, resource.length * 2);
+  io.position = 0;
+  io.write(new ByteString(resource));
+  checkCursors(io, resource.length, resource.length * 2);
+}
+
+function read(io) {
+  var position = io.position;
+  var length = io.length;
+  var bytes = io.read(resource.length);
+  assert.strictEqual(bytes.length, resource.length);
+  assert.strictEqual(bytes.decodeToString(), resource);
+  checkCursors(io, position + resource.length, length);
+}
+
+exports.testMemoryStreamEmpty = function() {
+  [new MemoryStream(), new MemoryStream(resource.length)].forEach(function(io) {
+    checkRW(io, true, true);
+    checkCursors(io, 0, 0);
+    io.position = 42;
+    checkCursors(io, 0, 0);
+    write(io);
+    read(io);
+    io.close();
+    noWrite(io);
+  });
+};
+
+exports.testMemoryStreamArray = function() {
+  var io = new MemoryStream(new ByteArray(resource));
+  checkRW(io, true, true);
+  checkCursors(io, 0, resource.length);
+  write(io);
+  read(io);
   io.close();
-  noWrite();
-  io = new MemoryStream(new ByteString(resource));
-  checkRW(true, false);
-  checkCursors(0, resource.length);
-  read();
-  noWrite();
+  noWrite(io);
+};
+
+exports.testMemoryStreamString = function() {
+  var io = new MemoryStream(new ByteString(resource));
+  checkRW(io, true, false);
+  checkCursors(io, 0, resource.length);
+  read(io);
+  noWrite(io);
+};
+
+exports.testTextStreamRead = function() {
+  var io = new TextStream(getStream('./lib/assert.js'));
+  var text = io.read();
+  assert.strictEqual(text.length, resource.length);
+  assert.strictEqual(text, resource);
+};
+
+exports.testTextStreamForEach = function() {
+  var resource = getContents('./lib/assert.js');
+  var io = new TextStream(getStream('./lib/assert.js'));
+  var trimmed = resource.replace(/\n$/, '');
+  var lines = trimmed.split(/\n/g);
+  var i = 0;
+  io.forEach(function(read) {
+    assert.strictEqual(read, lines[i]);
+    i++;
+  });
+  assert.strictEqual(i, lines.length);
+};
+
+exports.testTextStreamReadLine = function() {
+  var resource = getContents('./lib/assert.js');
+  var io = new TextStream(getStream('./lib/assert.js'));
+  var trimmed = resource.replace(/\n$/, '');
+  var lines = trimmed.split(/\n/g);
+  var last = resource === trimmed ? '' : '\n';
+  for (var i = 0; i < lines.length; i++) {
+    var line = lines[i] + (i === lines.length - 1 ? last : '\n');
+    assert.strictEqual(io.readLine(), line);
+  }
+  assert.strictEqual(io.readLine(), '');
+};
+
+exports.testTextStreamReadLines = function() {
+  var resource = getContents('./lib/assert.js');
+  var io = new TextStream(getStream('./lib/assert.js'));
+  var read = io.readLines();
+  var trimmed = resource.replace(/\n$/, '');
+  var lines = trimmed.split(/\n/g);
+  assert.strictEqual(read.length, lines.length);
+  var last = resource === trimmed ? '' : '\n';
+  for (var i = 0; i < lines.length; i++) {
+    var line = lines[i] + (i === lines.length - 1 ? last : '\n');
+    assert.strictEqual(read[i], line);
+  }
+};
+
+exports.testTextStreamWrite = function() {
+  var resource = getContents('./lib/assert.js');
+  var stream = new MemoryStream();
+  var io = new TextStream(stream);
+  io.write(resource);
+  assert.strictEqual(stream.content.decodeToString().length, resource.length);
+  assert.strictEqual(stream.content.decodeToString(), resource);
+};
+
+exports.testTextStreamWriteLine = function() {
+  var resource = getContents('./lib/assert.js');
+  var stream = new MemoryStream();
+  var io = new TextStream(stream);
+  var trimmed = resource.replace(/\n$/, '');
+  trimmed.split(/\n/g).forEach(function(line) {
+    io.writeLine(line);
+  });
+  resource = trimmed + '\n';
+  assert.strictEqual(stream.content.decodeToString().length, resource.length);
+  assert.strictEqual(stream.content.decodeToString(), resource);
+};
+
+exports.testTextStreamWriteLines = function() {
+  var resource = getContents('./lib/assert.js');
+  var stream = new MemoryStream();
+  var io = new TextStream(stream);
+  var trimmed = resource.replace(/\n$/, '');
+  io.writeLines(trimmed.split(/\n/g));
+  resource = trimmed + '\n';
+  assert.strictEqual(stream.content.decodeToString().length, resource.length);
+  assert.strictEqual(stream.content.decodeToString(), resource);
 };
 
 if (require.main === module) {
