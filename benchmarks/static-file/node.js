845bda30c292ff80d1d86324acf8c767b453d9bc
alexlamsl <alexlamsl@gmail.com>
2013-03-30 16:45:41 +0000

    #29

--- benchmarks/static-file/node.js
+++ benchmarks/static-file/node.js
@@ -1,3 +1,15 @@
-var connect = require('connect');
+var http = require('http');
+var fs = require('fs');
 
-connect(connect.static('../')).listen(8080);
+http.createServer(function(req, res) {
+  res.writeHead(200, {"Content-Type":"text/plain"});
+  var input = fs.createReadStream('../README.md').on('error', function() {
+    res.end();
+  }).on('end', function() {
+    res.end();
+  }).on('close', function() {
+    res.end();
+  }).on('readable', function() {
+    res.write(input.read());
+  });
+}).listen(8080);
