5f95d1d5714001b24614173bbf91a34a4b450e84
alexlamsl <alexlamsl@gmail.com>
2013-03-31 19:05:50 +0100

    fix HTTP client tests (need to connect() before read())

--- test/httpclient.js
+++ test/httpclient.js
@@ -2,33 +2,32 @@ var assert = require("../lib/assert");
 var HttpClient = require("../lib/httpclient").HttpClient;
 
 exports.testGet = function() {
-	var response = new HttpClient({
-		method: "GET",
-		url: "http://google.com/"
-	}).read();
-	assert.strictEqual(response.status, 301);
+  var response = new HttpClient({
+    method:"GET",
+    url:"http://google.com/"
+  }).finish();
+  assert.strictEqual(response.status, 301);
 };
 
 exports.testPut = function() {
-	var response = new HttpClient({
-		method: "PUT",
-		url: "http://google.com/"
-	}).read();
-	assert.strictEqual(response.status, 405);
+  var response = new HttpClient({
+    method:"PUT",
+    url:"http://google.com/"
+  }).finish();
+  assert.strictEqual(response.status, 405);
 };
 
 exports.testHttpsGet = function() {
-	var response = new HttpClient({
-		method: "GET",
-		url: "https://google.com/"
-	}).read();
-	assert.strictEqual(response.status, 301);
+  var response = new HttpClient({
+    method:"GET",
+    url:"https://google.com/"
+  }).finish();
+  assert.strictEqual(response.status, 301);
 };
 
 exports.testPutBody = function() {
-
 };
 
-if (require.main == module) {
-	require("../lib/test").run(exports);
+if (require.main === module) {
+  require("../lib/test").run(exports);
 }
