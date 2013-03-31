931fabe4b47c2064f254a9e81093ab020514ab32
alexlamsl <alexlamsl@gmail.com>
2013-03-31 12:22:31 +0100

    #62

--- test/fs-base/dirname.js
+++ test/fs-base/dirname.js
@@ -13,7 +13,7 @@ tests.forEach(function(dirs) {
 
 tests.forEach(function(dirs) {
   exports['testPath "' + dirs[0] + '"'] = function() {
-    var actual = new fs.Path(dirs[0]).directory();
+    var actual = fs.path(dirs[0]).directory();
     assert.strictEqual(dirs[1], actual.toString());
   };
 });
