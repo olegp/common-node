90158618500a49f74e613ac17101a3214fa01f90
alexlamsl <alexlamsl@gmail.com>
2013-03-31 12:46:32 +0100

    LINT

--- test/fs-base/relative.js
+++ test/fs-base/relative.js
@@ -4,20 +4,20 @@ var separator = fs.separator;
 
 var tests;
 tests = [['', '', ''], ['.', '', ''], ['', '.', ''], ['.', '.', ''],
-		['', '..', '../'], ['', '../', '../'], ['a', 'b', 'b'],
-		['../a', '../b', 'b'], ['../a/b', '../a/c', 'c'], ['a/b', '..', '../../'],
-		['a/b', 'c', '../c'], ['a/b', 'c/d', '../c/d'], ];
+  ['', '..', '../'], ['', '../', '../'], ['a', 'b', 'b'],
+  ['../a', '../b', 'b'], ['../a/b', '../a/c', 'c'], ['a/b', '..', '../../'],
+  ['a/b', 'c', '../c'], ['a/b', 'c/d', '../c/d'], ];
 
 tests.forEach(function(args) {
-	var source = args[0], target = args[1], expected = args[2];
-	var name = '"' + source + '" -> "' + target + '" = "' + expected + '"';
-	exports['test ' + name] = function() {
-		var actual = fs.relative(source, target);
-		// expect returned paths to use system-dependent file separator
-		assert.strictEqual(expected.replace(/\//g, separator), actual);
-	};
+  var source = args[0], target = args[1], expected = args[2];
+  var name = '"' + source + '" -> "' + target + '" = "' + expected + '"';
+  exports['test ' + name] = function() {
+    var actual = fs.relative(source, target);
+    // expect returned paths to use system-dependent file separator
+    assert.strictEqual(expected.replace(/\//g, separator), actual);
+  };
 });
 
-if(require.main == module) {
-	require("../../lib/test").run(exports);
+if (require.main === module) {
+  require("../../lib/test").run(exports);
 }
