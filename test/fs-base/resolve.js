5c0812bac48a2f2e7134101d7a21837b52d86338
alexlamsl <alexlamsl@gmail.com>
2013-03-31 12:48:24 +0100

    LINT

--- test/fs-base/resolve.js
+++ test/fs-base/resolve.js
@@ -2,29 +2,30 @@ var assert = require('../../lib/assert');
 var fs = require('../../lib/fs-base');
 var separator = fs.separator, roots = [separator];
 
-var tests;
-tests = [[['/'], '/'], [['/a'], '/a'], [['/a/'], '/a/'],
-		[['/a', '/b'], '/b'], [['/a', '/b/'], '/b/'], [['/', 'a'], '/a'],
-		[['/', 'a/'], '/a/'], [['/a', 'a'], '/a'], [['/a', 'a/'], '/a/'],
-		[['/a/', 'a'], '/a/a'], [['/a/', 'a/'], '/a/a/'], [['..'], '../'],
-		[['..', 'a'], '../a'], [['..', 'a/'], '../a/'], [['.'], ''],
-		[['.', 'a'], 'a'], [['.', 'a/'], 'a/'], [['a', '.'], ''],
-		[['a', '.', 'a'], 'a'], [['a', '.', 'a/'], 'a/'], [['a', '..'], '../'],
-		[['a', '..', 'a'], '../a'], [['a', '..', 'a/'], '../a/'],
-		[['a/', '..'], ''], [['a/', '..', 'a'], 'a'], [['a/', '..', 'a/'], 'a/'],
-		[['a/b', ''], 'a/b'], ];
+var tests = [
+  [['/'], '/'], [['/a'], '/a'], [['/a/'], '/a/'],
+  [['/a', '/b'], '/b'], [['/a', '/b/'], '/b/'], [['/', 'a'], '/a'],
+  [['/', 'a/'], '/a/'], [['/a', 'a'], '/a'], [['/a', 'a/'], '/a/'],
+  [['/a/', 'a'], '/a/a'], [['/a/', 'a/'], '/a/a/'], [['..'], '../'],
+  [['..', 'a'], '../a'], [['..', 'a/'], '../a/'], [['.'], ''],
+  [['.', 'a'], 'a'], [['.', 'a/'], 'a/'], [['a', '.'], ''],
+  [['a', '.', 'a'], 'a'], [['a', '.', 'a/'], 'a/'], [['a', '..'], '../'],
+  [['a', '..', 'a'], '../a'], [['a', '..', 'a/'], '../a/'],
+  [['a/', '..'], ''], [['a/', '..', 'a'], 'a'], [['a/', '..', 'a/'], 'a/'],
+  [['a/b', ''], 'a/b']
+];
 
 tests.forEach(function(args) {
-	exports['test ' + JSON.stringify(args[0])] = function() {
-		var result = fs.resolve.apply(null, args[0].map(localize));
-		assert.strictEqual(localize(args[1]), result);
-	};
+  exports['test ' + JSON.stringify(args[0])] = function() {
+    var result = fs.resolve.apply(null, args[0].map(localize));
+    assert.strictEqual(localize(args[1]), result);
+  };
 });
 
 function localize(path) {
-	return path.replace(/^\//, roots[0]).replace(/\//g, separator);
+  return path.replace(/^\//, roots[0]).replace(/\//g, separator);
 }
 
-if(require.main == module) {
-	require("../../lib/test").run(exports);
+if (require.main === module) {
+  require("../../lib/test").run(exports);
 }
