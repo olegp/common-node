05838cb4ab1d7b44f3b1d65717b0ee93cf6a8cba
alexlamsl <alexlamsl@gmail.com>
2013-03-31 12:46:15 +0100

    #62

--- test/fs-base/normal.js
+++ test/fs-base/normal.js
@@ -4,21 +4,28 @@ var separator = fs.separator;
 
 var tests;
 tests = [['', ''], ['.', ''], ['./', ''], ['../', '../'], ['../a', '../a'],
-		['../a/', '../a/'], ['a/..', ''], ['a/../', ''], ['a/../b', 'b'],
-		['a/../b/', 'b/'], ];
+  ['../a/', '../a/'], ['a/..', ''], ['a/../', ''], ['a/../b', 'b'],
+  ['a/../b/', 'b/'], ];
 
 tests.forEach(function(args) {
-	exports['test "' + args[0] + '"'] = function() {
-		var result = fs.normal(localize(args[0]));
-		assert.strictEqual(localize(args[1]), result);
-	};
+  exports['test "' + args[0] + '"'] = function() {
+    var result = fs.normal(localize(args[0]));
+    assert.strictEqual(localize(args[1]), result);
+  };
+});
+
+tests.forEach(function(args) {
+  exports['testPath "' + args[0] + '"'] = function() {
+    var result = fs.path(localize(args[0])).normal();
+    assert.strictEqual(localize(args[1]), result.toString());
+  };
 });
 
 // adapt path to the platform we're running on
 function localize(path) {
-	return path.replace(/\//g, separator);
+  return path.replace(/\//g, separator);
 }
 
-if(require.main == module) {
-	require("../../lib/test").run(exports);
+if (require.main === module) {
+  require("../../lib/test").run(exports);
 }
