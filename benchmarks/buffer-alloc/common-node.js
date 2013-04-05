94383de7f41740ce56c29205240a0923bf4478ca
alexlamsl <alexlamsl@gmail.com>
2013-04-05 14:21:00 +0100

    revise benchmark parameters

--- benchmarks/buffer-alloc/common-node.js
+++ benchmarks/buffer-alloc/common-node.js
@@ -1,14 +1,15 @@
 var ByteArray = require('binary').ByteArray;
 
 exports.app = function(request) {
-	var n = 1024, b = new ByteArray(n);
-	for( var i = 1; i < 50; i++)
-		b = new ByteArray(n, true);
-	return {
-		status: 200,
-		headers: {
-			"Content-Type": "text/plain"
-		},
-		body: [b]
-	};
+  var n = 1024, b;
+  for (var i = 0; i < 1024; i++) {
+    b = new ByteArray(n);
+  }
+  return {
+    status:200,
+    headers:{
+      "Content-Type":"text/plain"
+    },
+    body:[b]
+  };
 };
--- benchmarks/buffer-alloc/node.js
+++ benchmarks/buffer-alloc/node.js
@@ -2,13 +2,13 @@ var http = require('http');
 var Buffer = require('buffer').Buffer;
 
 http.createServer(function(req, res) {
-	var n = 1024, b = new Buffer(n);
-	for( var i = 1; i < 50; i++) {
-		b = new Buffer(n);
-		b.fill(0);
+  var n = 1024, b;
+  for (var i = 0; i < 1024; i++) {
+    var b = new Buffer(n);
+    b.fill(0);
   }
-	res.writeHead(200, {
-		"Content-Type": "text/plain"
-	});
-	res.end(b);
+  res.writeHead(200, {
+    "Content-Type":"text/plain"
+  });
+  res.end(b);
 }).listen(8080);
